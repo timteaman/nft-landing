9077e0e2988e5cea983aef4a9e61438ad1408b3b
timteaman <tim.like.tea@gmail.com>
2024-02-18 21:11:08 +0100

    fix variables and fix routing

--- src/modules/header/Header.jsx
+++ src/modules/header/Header.jsx
@@ -1,22 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './styles.module.scss';
 import AuthButton from '@components/authButton/AuthButton';
 import Logo from '@shared/Logo/Logo';
 
 function Header() {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <header className={styles.header}>
       <nav className={styles.menu}>
         <Logo alt="conpany logo" width="243" height="32" />
 
         <ul className={styles.menu__list}>
-          <li className={styles.menu__item}>
+          <li
+            className={`${styles.menu__item} ${
+              isActive('/marketplace') ? styles.active : ''
+            }`}
+          >
             <Link to="/marketplace">Marketplace</Link>
           </li>
-          <li className={styles.menu__item}>
+          <li
+            className={`${styles.menu__item} ${
+              isActive('/rankings') ? styles.active : ''
+            }`}
+          >
             <Link to="/rankings">Rankings</Link>
           </li>
-          <li className={styles.menu__item}>
+          <li
+            className={`${styles.menu__item} ${
+              isActive('/connect-wallet') ? styles.active : ''
+            }`}
+          >
             <Link to="/connect-wallet">Connect a wallet</Link>
           </li>
         </ul>
